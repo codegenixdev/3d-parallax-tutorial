c109c2eed7d5b2ced4d37427aa759d750ec048b5
amirnz026 <czhablog@gmail.com>
2024-08-04 20:23:43 +0330

    Minor fixes

--- src/3d-hover.tsx
+++ src/3d-hover.tsx
@@ -1,17 +1,5 @@
-import React, { createContext, useContext, useRef, useState } from "react";
+import React from "react";
 import { cn } from "./utils/cn";
-
-const MouseEnterContext = createContext<
-  [boolean, React.Dispatch<React.SetStateAction<boolean>>] | undefined
->(undefined);
-
-const useMouseEnter = () => {
-  const context = useContext(MouseEnterContext);
-  if (context === undefined) {
-    throw new Error("useMouseEnter must be used within a MouseEnterProvider");
-  }
-  return context;
-};
 
 export const CardContainer = ({
   children,
@@ -20,18 +8,14 @@ export const CardContainer = ({
   children?: React.ReactNode;
   className?: string;
 }) => {
-  const [isMouseEntered, setIsMouseEntered] = useState(false);
-
   return (
-    <MouseEnterContext.Provider value={[isMouseEntered, setIsMouseEntered]}>
-      <div className="flex items-center justify-center py-20">
-        <div
-          className={cn("relative flex items-center justify-center", className)}
-        >
-          {children}
-        </div>
+    <div className="flex items-center justify-center py-20">
+      <div
+        className={cn("relative flex items-center justify-center", className)}
+      >
+        {children}
       </div>
-    </MouseEnterContext.Provider>
+    </div>
   );
 };
 
@@ -56,10 +40,8 @@ export const CardItem = ({
   className?: string;
   [key: string]: unknown;
 }) => {
-  const ref = useRef<HTMLDivElement>(null);
-
   return (
-    <Component ref={ref} className={className} {...rest}>
+    <Component className={cn("w-fit", className)} {...rest}>
       {children}
     </Component>
   );
